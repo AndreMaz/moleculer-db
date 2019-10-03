f666b2acfcaa434ddee2f9c714d65f6d2be9a0c9
AndreMaz <andre.maz.90@gmail.com>
2019-10-03 14:30:46 +0100

    [WIP] more methods

--- packages/moleculer-db-adapter-cosmos/examples/simple/index.js
+++ packages/moleculer-db-adapter-cosmos/examples/simple/index.js
@@ -33,6 +33,11 @@ broker.createService(StoreService, {
 		// console.log(res);
 		// let idList = ["5b774410-0141-4964-8178-f4deb38cea08", "f13847a7-ff51-4be6-b782-a596b7af6ca5"]
 		// this.adapter.findByIds(idList);
+
+		let entries = ["f13847a7-ff51-4be6-b782-a596b7af6ca5"];
+
+		let result = await this.adapter.clear(entries);
+		console.log(result);
 	}
 });
 
@@ -48,14 +53,28 @@ broker.start().then(async () => {
 		});
 		*/
 		// console.log(result);
-
+		/*
 		let idList = [
 			"5b774410-0141-4964-8178-f4deb38cea08",
 			"f13847a7-ff51-4be6-b782-a596b7af6ca5"
 		];
 
-		let result = await broker.call("posts.get", { id: idList });
+		let result = await broker.call("posts.count");
 		console.log(result);
+		*/
+		/*
+		let entities = [{ a: "Hello 1" }, { b: "Hello 2" }];
+		let result = await broker.call("posts.insert", { entities: entities });
+		console.log(result);
+		*/
+		/*
+		let params = {
+			id: "f908e7b4-bd1f-4767-8c06-821f822bb346",
+			b: "Hello UPDATED HANDLER"
+		};
+		let result = await broker.call("posts.update", params);
+		console.log(result);
+		*/
 	} catch (error) {
 		console.log(error);
 	}
--- packages/moleculer-db-adapter-cosmos/src/index.js
+++ packages/moleculer-db-adapter-cosmos/src/index.js
@@ -72,6 +72,8 @@ class CosmosDbAdapter {
 				this.containerName
 			);
 			this.container = containerResponse.container;
+
+			return Promise.resolve();
 		} catch (error) {
 			throw error;
 		}
@@ -85,18 +87,54 @@ class CosmosDbAdapter {
 	 * @memberof CosmosDbAdapter
 	 */
 	disconnect() {
+		// We are making REST requests so just resolve
 		return Promise.resolve();
 	}
 
+	/**
+	 * Find all entities by filters.
+	 *
+	 *  - query
+	 *
+	 * @param {any} filters
+	 * @returns {Promise}
+	 *
+	 * @memberof CosmosDbAdapter
+	 */
 	find(filters) {
 		// throw new Error("Method Not Implemented");
 		return this.createCursor(filters);
 	}
 
-	findOne(query) {
-		throw new Error("Method `findOne` Not Implemented");
+	/**
+	 * Find an entity by query
+	 *
+	 * @param {Object} query
+	 * @returns {Promise}
+	 *
+	 * @memberof MemoryDbAdapter
+	 */
+	async findOne(query) {
+		// throw new Error("Method `findOne` Not Implemented");
+		try {
+			const response = await this.container.items.query(query).fetchAll;
+
+			if (response.statusCode > 400) return;
+
+			return response.resource;
+		} catch (error) {
+			throw error;
+		}
 	}
 
+	/**
+	 * Find an entities by ID
+	 *
+	 * @param {any} _id
+	 * @returns {Promise}
+	 *
+	 * @memberof CosmosDbAdapter
+	 */
 	async findById(_id) {
 		// throw new Error("Method `findById` Not Implemented");
 		try {
@@ -110,8 +148,12 @@ class CosmosDbAdapter {
 	}
 
 	/**
+	 * Find any entities by IDs
 	 *
 	 * @param {Array} idList
+	 * @returns {Promise}
+	 *
+	 * @memberof CosmosDbAdapter
 	 */
 	async findByIds(idList) {
 		// throw new Error("Method `findByIds` Not Implemented");
@@ -126,10 +168,30 @@ class CosmosDbAdapter {
 		return response.resources;
 	}
 
+	/**
+	 * Get count of filtered entities
+	 *
+	 * Available filter props:
+	 *  - query
+	 *
+	 * @param {Object} [filters={}]
+	 * @returns {Promise}
+	 *
+	 * @memberof CosmosDbAdapter
+	 */
 	count(filters = {}) {
-		throw new Error("Method `count` Not Implemented");
+		// throw new Error("Method `count` Not Implemented");
+		return this.createCursor(filters, true);
 	}
 
+	/**
+	 * Insert an entity
+	 *
+	 * @param {Object} entity
+	 * @returns {Promise}
+	 *
+	 * @memberof CosmosDbAdapter
+	 */
 	async insert(entity) {
 		try {
 			const response = await this.container.items.create(entity);
@@ -139,22 +201,95 @@ class CosmosDbAdapter {
 		}
 	}
 
+	/**
+	 * Insert many entities
+	 *
+	 * @param {Array} entities
+	 * @returns {Promise}
+	 *
+	 * @memberof CosmosDbAdapter
+	 */
 	insertMany(entities) {
-		throw new Error("Method `insertMany` Not Implemented");
+		// At this moment there is no Bulk Executor Lib for Node
+		// More info: https://docs.microsoft.com/en-us/azure/cosmos-db/bulk-executor-overview
+
+		// ToDo: Check CosmosDB DB engine stored procedures
+		// More info: https://docs.microsoft.com/en-us/azure/cosmos-db/stored-procedures-triggers-udfs
+		this.broker.logger.warn(
+			"insertMany() method is not Request Units (RUs) friendly. Use it at your own risk. Consider using CosmosDB DB stored procedures"
+		);
+
+		const p = entities.map(entity => this.insert(entity));
+
+		try {
+			const response = Promise.all(p);
+			return response;
+		} catch (error) {
+			throw error;
+		}
 	}
 
-	updateMany(query, update) {
-		throw new Error("Method `updateMany` Not Implemented");
+	/**
+	 * Update an entity by ID and `update`
+	 *
+	 * @param {any} _id
+	 * @param {Object} update
+	 * @returns {Promise}
+	 *
+	 * @memberof CosmosDbAdapter
+	 */
+	async updateById(_id, update) {
+		// throw new Error("Method `updateById` Not Implemented");
+		try {
+			const newItem = { id: _id, ...update["$set"] };
+
+			const response = await this.container.item(_id).replace(newItem);
+
+			if (response.statusCode >= 400) return;
+
+			return response.resource;
+		} catch (error) {
+			throw error;
+		}
 	}
 
-	updateById(_id, update) {
-		throw new Error("Method `updateById` Not Implemented");
+	/**
+	 * Update many entities
+	 *
+	 * @param {Array} update array of entities
+	 * @returns {Promise}
+	 *
+	 * @memberof CosmosDbAdapter
+	 */
+	updateMany(entries) {
+		// At this moment there is no Bulk Executor Lib for Node
+		// More info: https://docs.microsoft.com/en-us/azure/cosmos-db/bulk-executor-overview
+
+		// ToDo: Check CosmosDB DB stored procedures
+		// More info: https://docs.microsoft.com/en-us/azure/cosmos-db/stored-procedures-triggers-udfs
+		this.broker.logger.warn(
+			"updateMany() method operator is not Request Units (RUs) friendly. Use it at your own risk. Consider using CosmosDB DB stored procedures"
+		);
+		try {
+			const p = entries.map(entry =>
+				this.container.item(entry.id).replace(entry)
+			);
+
+			const response = Promise.all(p);
+			return response.map(entry => entry.resource);
+		} catch (error) {
+			throw error;
+		}
 	}
 
-	removeMany(query) {
-		throw new Error("Method `removeMany` Not Implemented");
-	}
-
+	/**
+	 * Remove an entity by ID
+	 *
+	 * @param {any} _id
+	 * @returns {Promise}
+	 *
+	 * @memberof CosmosDbAdapter
+	 */
 	async removeById(_id) {
 		try {
 			const readResponse = await this.container.item(_id).read();
@@ -163,50 +298,136 @@ class CosmosDbAdapter {
 			const delResponse = await this.container.item(_id).delete();
 			if (delResponse.statusCode === 404) return;
 
-			return response.resource;
+			return readResponse.resource;
 		} catch (error) {
 			throw error;
 		}
 	}
 
-	clear() {
-		throw new Error("Method `clear` Not Implemented");
+	/**
+	 * Remove many entities by their ID
+	 *
+	 * @param {Array<string>} idList
+	 * @returns {Promise}
+	 *
+	 * @memberof CosmosDbAdapter
+	 */
+	removeMany(idList) {
+		// At this moment there is no Bulk Executor Lib for Node
+		// More info: https://docs.microsoft.com/en-us/azure/cosmos-db/bulk-executor-overview
+
+		// ToDo: Check CosmosDB DB engine stored procedures
+		// More info: https://docs.microsoft.com/en-us/azure/cosmos-db/stored-procedures-triggers-udfs
+		this.broker.logger.warn(
+			"removeMany() method is not Request Units (RUs) friendly. Use it at your own risk. Consider using CosmosDB DB stored procedures"
+		);
+
+		try {
+			const p = idList.map(id => this.removeById(id));
+			return Promise.all(p);
+		} catch (error) {
+			throw error;
+		}
 	}
 
+	/**
+	 * Clear all entities from container
+	 *
+	 * @returns {Promise}
+	 *
+	 * @memberof CosmosDbAdapter
+	 */
+	async clear() {
+		// At this moment there is no Bulk Executor Lib for Node
+		// More info: https://docs.microsoft.com/en-us/azure/cosmos-db/bulk-executor-overview
+
+		// ToDo: Check CosmosDB DB engine stored procedures
+		// More info: https://docs.microsoft.com/en-us/azure/cosmos-db/stored-procedures-triggers-udfs
+		this.broker.logger.warn(
+			"clear() method is not Request Units (RUs) friendly. Use it at your own risk. Consider using CosmosDB DB stored procedures"
+		);
+
+		try {
+			const allEntries = await this.find();
+
+			const result = await this.removeMany(
+				allEntries.map(entry => entry.id)
+			);
+
+			return result;
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	/**
+	 * Convert DB entity to JSON object
+	 *
+	 * @param {any} entity
+	 * @returns {Object}
+	 * @memberof CosmosDbAdapter
+	 */
 	entityToObject(entity) {
-		// throw new Error("Method Not Implemented");
 		return entity;
 	}
 
+	/**
+	 * Create a filtered query
+	 * Available filters in `params`:
+	 *  - query
+	 *
+	 * @param {Object} params
+	 * @param {Boolean} isCounting
+	 * @returns {Promise}
+	 */
 	async createCursor(params = {}, isCounting) {
 		const q = {
 			query: params.query || "SELECT * from c", // By default get everything
 			parameters: []
 		};
 
+		if (isCounting) {
+			q.query = "SELECT VALUE COUNT(1) FROM c"; // Count all entries
+		}
+
 		const response = await this.container.items.query(q).fetchAll();
+
+		// Response is an array with the count number
+		if (isCounting) return response.resources[0];
+
 		return response.resources;
 	}
 
+	/**
+	 * Convert the `sort` param to a `sort` object to Sequelize queries.
+	 *
+	 * @param {String|Array<String>|Object} paramSort
+	 * @returns {Object} Return with a sort object like `[["votes", "ASC"], ["title", "DESC"]]`
+	 * @memberof CosmosDbAdapter
+	 */
 	transformSort(paramSort) {
 		throw new Error("Method `transformSort` Not Implemented");
 	}
 
-	stringToObjectID(id) {
-		throw new Error("Method `stringToObjectID` Not Implemented");
-	}
-
-	objectIDToString(id) {
-		throw new Error("Method `objectIDToString` Not Implemented");
-	}
-
+	/**
+	 * For compatibility only.
+	 * @param {Object} entity
+	 * @param {String} idField
+	 * @memberof CosmosDbAdapter
+	 * @returns {Object} Entity
+	 */
 	beforeSaveTransformID(entity, idField) {
 		return entity;
-		// throw new Error("Method `beforeSaveTransformID` Not Implemented");
 	}
 
+	/**
+	 * For compatibility only.
+	 * @param {Object} entity
+	 * @param {String} idField
+	 * @memberof CosmosDbAdapter
+	 * @returns {Object} Entity
+	 */
 	afterRetrieveTransformID(entity, idField) {
-		// throw new Error("Method Not Implemented");
 		return entity;
 	}
 }
